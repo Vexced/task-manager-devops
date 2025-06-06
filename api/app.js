aa7e207ad082ec0f4409d4952546725ca7023f4c
Vexced <axel.vergara@mail.com>
2025-06-05 18:05:51 -0600

    prueba 3

--- api/app.js
+++ api/app.js
@@ -24,14 +24,11 @@ app.get('/', (req, res) => {
   res.send('API funcionando correctamente');
 });
 
-app.post('/github-webhook/', (req, res) => {
+app.post('/github-webhook', (req, res) => {
   console.log('Payload recibido:', req.body);
-
-  // Aquí puedes hacer lo que necesites con la información, por ejemplo:
-  // validar el evento, ejecutar un script, disparar un CI/CD, etc.
-
   res.status(200).send('Webhook recibido correctamente');
 });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor escuchando en puerto ${port}`);
